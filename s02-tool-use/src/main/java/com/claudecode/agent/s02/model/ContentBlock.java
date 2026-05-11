6f5e7e49dde13c62019dab8ada2d3d75a95e5ef3
wsh <wangsihan@yyr234.com>
2026-05-11 15:37:12 +0800

    Fix ContentBlock constructor conflict in s02, s04, s06, sfull modules
    - Remove @Builder, @NoArgsConstructor, @AllArgsConstructor from abstract ContentBlock class
    - These annotations should only be on the inner static classes
    - Add @JsonIgnoreProperties(ignoreUnknown = true) to inner classes
    - Add ThinkingBlock type for extended model support
    - Align all modules with s01 correct implementation

--- s02-tool-use/src/main/java/com/claudecode/agent/s02/model/ContentBlock.java
+++ s02-tool-use/src/main/java/com/claudecode/agent/s02/model/ContentBlock.java
@@ -1,5 +1,6 @@
 package com.claudecode.agent.s02.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
@@ -12,13 +13,11 @@ import lombok.NoArgsConstructor;
 import java.util.Map;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, defaultImpl = ContentBlock.TextBlock.class)
 @JsonSubTypes({
     @JsonSubTypes.Type(value = ContentBlock.TextBlock.class, name = "text"),
+    @JsonSubTypes.Type(value = ContentBlock.ThinkingBlock.class, name = "thinking"),
     @JsonSubTypes.Type(value = ContentBlock.ToolUseBlock.class, name = "tool_use"),
     @JsonSubTypes.Type(value = ContentBlock.ToolResultBlock.class, name = "tool_result")
 })
@@ -28,6 +27,7 @@ public abstract class ContentBlock {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class TextBlock extends ContentBlock {
         private String type = "text";
@@ -38,6 +38,19 @@ public abstract class ContentBlock {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class ThinkingBlock extends ContentBlock {
+        private String type = "thinking";
+        private String thinking;
+        private String signature;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class ToolUseBlock extends ContentBlock {
         private String type = "tool_use";
@@ -50,6 +63,7 @@ public abstract class ContentBlock {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class ToolResultBlock extends ContentBlock {
         private String type = "tool_result";
--- s04-subagent/src/main/java/com/claudecode/agent/s04/model/ContentBlock.java
+++ s04-subagent/src/main/java/com/claudecode/agent/s04/model/ContentBlock.java
@@ -1,7 +1,10 @@
 package com.claudecode.agent.s04.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,16 +13,22 @@ import lombok.NoArgsConstructor;
 import java.util.Map;
 
 @Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION, defaultImpl = ContentBlock.TextBlock.class)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = ContentBlock.TextBlock.class, name = "text"),
+    @JsonSubTypes.Type(value = ContentBlock.ThinkingBlock.class, name = "thinking"),
+    @JsonSubTypes.Type(value = ContentBlock.ToolUseBlock.class, name = "tool_use"),
+    @JsonSubTypes.Type(value = ContentBlock.ToolResultBlock.class, name = "tool_result")
+})
 public abstract class ContentBlock {
 
     @Data
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class TextBlock extends ContentBlock {
         private String type = "text";
         private String text;
@@ -29,6 +38,20 @@ public abstract class ContentBlock {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class ThinkingBlock extends ContentBlock {
+        private String type = "thinking";
+        private String thinking;
+        private String signature;
+    }
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class ToolUseBlock extends ContentBlock {
         private String type = "tool_use";
         private String id;
@@ -40,6 +63,8 @@ public abstract class ContentBlock {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class ToolResultBlock extends ContentBlock {
         private String type = "tool_result";
         @JsonProperty("tool_use_id")
